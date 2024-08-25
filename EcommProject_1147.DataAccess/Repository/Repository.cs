a15fa31bfa23df16ddf6984cc67683363a793efa
Harindersidhu007 <harindersidhu071@gmail.com>
2024-08-26 00:07:02 +0530

    create repository

--- EcommProject_1147.DataAccess/Repository/Repository.cs
+++ EcommProject_1147.DataAccess/Repository/Repository.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using EcommProject_1147.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EcommProject_1147.DataAccess.Repository
 {
-    public class Repository<T>:IRepository <T> where T : class
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+        public Repository(DbContext dbContext, DbSet<T> dbSet)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbSet;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> Filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetValue(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

283bb602cad49fd62ed76e94c39d86103909ff4f
Harindersidhu007 <harindersidhu071@gmail.com>
2024-08-26 00:03:26 +0530

    Add project files.

---
+++ EcommProject_1147.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommProject_1147.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T>where T : class
+    {
+        void Add(T entity); 
+        void Update(T entity);  
+        void Remove(T entity);
+        void Remove(int id);
+        void RemoveRange(IEnumerable<T> entities);  
+        T GetValue(int id);
+        IEnumerable<T> GetAll(
+            Expression<Func<T,bool>> filter=null,
+            Func<IQueryable<T>,IOrderedQueryable<T>>OrderBy=null,
+            String includeProperties=null
+            );
+        T FirstOrDefault(
+            Expression<Func<T,bool>>Filter=null,
+            String includeProperties = null
+            );
+    }
+}
---
+++ EcommProject_1147.DataAccess/Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommProject_1147.DataAccess.Repository
+{
+    public class Repository<T>:IRepository <T> where T : class
+    {
+    }
+}

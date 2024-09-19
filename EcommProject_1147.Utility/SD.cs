a8f02c1d11f84b8b1b5175f8bf5e58996f2103e0
Harindersidhu007 <harindersidhu071@gmail.com>
2024-09-20 00:13:01 +0530

    Define store procedure and user roles in standard dictionary.

---
+++ EcommProject_1147.Utility/SD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommProject_1147.Utility
+{
+    public static class SD
+    {
+        //Cover Type SP
+        public const string Proc_GetCoverTypes = "GetCoverTypes";
+        public const string Proc_GetCoverType = "GetCoverType";
+        public const string Proc_CreateCoverType = "CreateCoverType";
+        public const string Proc_UpdateCoverType = "UpdateCoverType";
+        public const string Proc_DeleteCoverType = "DeleteCoverType";
+        //Roles
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee User";
+        public const string Role_Company = "Company User";
+        public const string Role_Individual = "Individual User";
+    }
+}
